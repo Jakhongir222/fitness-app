5c741a13c29a0c998cd614fbc33b9e65bd58d210
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-05 22:15:19 +0100

    Routing is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Monday } from './weekdays/Monday';
+import { Tuesday } from './weekdays/Tuesday';
+import { Wednesday } from './weekdays/Wednesday';
+import { Thursday } from './weekdays/Thursday';
+import { Friday } from './weekdays/Friday';
+import { Saturday } from './weekdays/Saturday';
+import { Sunday } from './weekdays/Sunday';
+import { Weekdays } from './weekdays/Weekdays';
 import './App.css';
+import { NotFound } from './weekdays/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <h1>Jak's fitness app</h1>
+        <Routes>
+          <Route path='/' element={<Weekdays/>} />
+          <Route path='/monday' element={<Monday/>} />
+          <Route path='/tuesday' element={<Tuesday/>} />
+          <Route path='/wednesday' element={<Wednesday/>} />
+          <Route path='/thursday' element={<Thursday/>} />
+          <Route path='/friday' element={<Friday/>} />
+          <Route path='/saturday' element={<Saturday/>} />
+          <Route path='/sunday' element={<Sunday/>} />
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
