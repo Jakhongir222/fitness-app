e1421490f2a8c8ad0174ad277a18d5c5b8b85c19
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-10 20:07:44 +0100

    Added the monday

--- src/weekdays/Monday.jsx
+++ src/weekdays/Monday.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { MondayCart } from './carts/MondayCart'
 
 export const Monday = () => {
   return (
     <div>
-      <p>30 hour cardio</p>
-      <p>Chest and triceps</p>
+      <h2>30 hour cardio</h2>
+      <h2>Chest and triceps</h2>
+      <MondayCart/>
+      <h2>Cool-Down and Stretch</h2>
       <iframe 
         width="560" 
         height="315" 
@@ -12,7 +15,7 @@ export const Monday = () => {
         title="Monday Video"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-              ></iframe>
+      ></iframe>
     </div>
   )
 }
--- src/weekdays/carts/MondayCart.jsx
+++ src/weekdays/carts/MondayCart.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const MondayCart = () => {
   return (
-    <div>MondayCart</div>
+    <div>
+        <h2> Chest wrokout </h2>
+        <p> Bench Press (3 sets x 8-12 reps) </p>
+        <p> Dumbbell Flyes (3 sets x 8-12 reps) </p>
+        <p> Push-Ups (3 sets x 8-12 reps) </p>
+        <h2> Triceps Workout </h2>
+        <p> Triceps Dips (3 sets x 8-12 reps) </p>
+        <p> Overhead Triceps Extension (3 sets x 8-12 reps) </p>
+        <p> Cable Triceps Pushdowns (3 sets x 8-12 reps) </p>
+    </div>
   )
 }
