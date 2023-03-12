fb9fd947f9f10e4e8ac5ad6eed9c91f4d4f6095b
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-13 00:09:44 +0100

    Styled the carts

--- src/weekdays/carts/MondayCart.jsx
+++ src/weekdays/carts/MondayCart.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import './Cart.css'
 
 export const MondayCart = () => {
   return (
-    <div>
+    <div className="workout-card">
         <h3> Chest Workout </h3>
+        <img src='https://www.inspireusafoundation.org/wp-content/uploads/2022/03/barbell-wide-grip-bench-press.gif' alt='Bench press'/>
         <p> Bench Press (3 sets x 8-12 reps) </p>
+        <img src='http://newlife.com.cy/wp-content/uploads/2019/11/03021301-Dumbbell-Decline-Fly_Chest_360.gif' alt='Dumbbell Flyes'/>
         <p> Dumbbell Flyes (3 sets x 8-12 reps) </p>
+        <img src='https://www.inspireusafoundation.org/wp-content/uploads/2022/05/weighted-push-up.gif' alt='Pushups' />
         <p> Push-Ups (3 sets x 8-12 reps) </p>
         <h3> Triceps Workout </h3>
+        <img src='https://fitnessprogramer.com/wp-content/uploads/2021/04/Triceps-Dips-on-Floor.gif' alt='Tricep dip' />
         <p> Triceps Dips (3 sets x 8-12 reps) </p>
+        <img src='http://newlife.com.cy/wp-content/uploads/2019/12/21881301-Dumbbell-Seated-Triceps-Extension_Upper-Arms_360.gif' alt='Overhead tricep' />
         <p> Overhead Triceps Extension (3 sets x 8-12 reps) </p>
+        <img src='https://www.inspireusafoundation.org/wp-content/uploads/2021/10/cable-tricep-pushdown.gif' alt='Hercules'/>
         <p> Cable Triceps Pushdowns (3 sets x 8-12 reps) </p>
     </div>
   )
