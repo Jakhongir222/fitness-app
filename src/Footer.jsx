43a65cd235c5142b0840162af07413783a0f1ed7
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-04-01 19:32:50 +0200

    Added the icon

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Footer.css';
 
 export const Footer = () => {
   return (
-    <div>
-      Powered by Jakhongir<span role="img" aria-label="Flexed biceps"> 💪</span>
+    <div className="footer">
+      © Copyright 2023 Jakhongir Burkhonov.
+      All rights reserved. <span role="img" aria-label="Flexed biceps">💪</span>
     </div>
   );
 };
