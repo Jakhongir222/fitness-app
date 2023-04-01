1bf696b97d4f6f5926dfde47d8370ba94d61c284
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-04-01 19:59:25 +0200

    Changed the buttons

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,8 +4,7 @@ import './Footer.css';
 export const Footer = () => {
   return (
     <div className="footer">
-      © Copyright 2023 Jakhongir Burkhonov.
-      All rights reserved. <span role="img" aria-label="Flexed biceps">💪</span>
+      ©Powered by Jakhongir <span role="img" aria-label="Flexed biceps">💪</span>
     </div>
   );
 };
